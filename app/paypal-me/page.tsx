2f73386323866e75a8d5593cf0382418a1db91e4
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-09-20 01:38:02 +0300

    added paypal page

---
+++ app/paypal-me/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Page = () => {
+  return (
+    <div>
+      Really sorry for doing this. But This is the only last minute solution i
+      could think off.
+      <p>
+        You can just treat my submission as any other submission and if i win i
+        will request the cash using this option
+      </p>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/paypal-me.png" alt="" className="w-1/2 aspect-video my-10" />
+      <p>
+        Hope it wont be a problem. There wll be no legal ramifications of any
+        sort. Im sorry for bothering you. I just assumed that since paypal is in
+        my country, paypal.me would be too but turns out nope. And this is after
+        completing the whole project
+      </p>
+    </div>
+  );
+};
+
+export default Page;
