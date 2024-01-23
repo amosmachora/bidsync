9be806bb9af687d868a7010f0d9e7de34dbe20c4
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2024-01-23 17:30:58 +0300

    commented out the whole crons.ts file

--- convex/crons.ts
+++ convex/crons.ts
@@ -1,8 +1,8 @@
-import { cronJobs } from "convex/server";
-import { internal } from "./_generated/api";
-import { internalMutation } from "./_generated/server";
+// import { cronJobs } from "convex/server";
+// import { internal } from "./_generated/api";
+// import { internalMutation } from "./_generated/server";
 
-const crons = cronJobs();
+// const crons = cronJobs();
 
 // crons.interval(
 //   "decrease stage time",
@@ -10,26 +10,26 @@ const crons = cronJobs();
 //   internal.crons.decreaseStageTime
 // );
 
-export const decreaseStageTime = internalMutation({
-  args: {},
-  handler: async ({ db }, {}) => {
-    const onStageItems = await db
-      .query("stageitems")
-      .filter((q) => {
-        return q.eq(q.field("isOnStage"), true);
-      })
-      .collect();
+// export const decreaseStageTime = internalMutation({
+//   args: {},
+//   handler: async ({ db }, {}) => {
+//     const onStageItems = await db
+//       .query("stageitems")
+//       .filter((q) => {
+//         return q.eq(q.field("isOnStage"), true);
+//       })
+//       .collect();
 
-    for (const onStageItem of onStageItems) {
-      if (onStageItem.onStageDuration! <= 0) {
-        await db.patch(onStageItem._id, { isOnStage: false });
-        return;
-      }
-      await db.patch(onStageItem?._id, {
-        onStageDuration: onStageItem.onStageDuration! - 1,
-      });
-    }
-  },
-});
+//     for (const onStageItem of onStageItems) {
+//       if (onStageItem.onStageDuration! <= 0) {
+//         await db.patch(onStageItem._id, { isOnStage: false });
+//         return;
+//       }
+//       await db.patch(onStageItem?._id, {
+//         onStageDuration: onStageItem.onStageDuration! - 1,
+//       });
+//     }
+//   },
+// });
 
-export default crons;
+// export default crons;
